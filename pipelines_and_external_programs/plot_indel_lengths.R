d2b07afbe62672e4171012e71f7acd547a691f71
henryjuho <juhob@hotmail.co.uk>
2017-05-30 12:23:49 +0100

    added plotting script

---
+++ pipelines_and_external_programs/plot_indel_lengths.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+
+args <- commandArgs(TRUE)
+csv_in = args[1]
+png_out = args[2]
+
+length_data <- read.csv(csv_in, header=FALSE)
+
+len_plot = ggplot(length_data, aes(x=V1, y=V2))+
+            geom_bar(stat='identity', position='dodge', fill='steel blue') +
+            xlab('Indel Length (bp)') + ylab('Number of INDELs') +
+            #xlim(0, 20) +
+            theme_bw()
+
+ggsave(png_out, len_plot, height=6, width=9)
