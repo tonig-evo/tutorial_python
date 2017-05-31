2bb43840312960a8f7900f84e6d9c6e58e21695c
henryjuho <juhob@hotmail.co.uk>
2017-05-31 08:16:50 +0100

    added xlim

--- pipelines_and_external_programs/plot_indel_lengths.R
+++ pipelines_and_external_programs/plot_indel_lengths.R
@@ -9,7 +9,7 @@ length_data <- read.csv(csv_in, header=FALSE)
 len_plot = ggplot(length_data, aes(x=V1, y=V2))+
             geom_bar(stat='identity', position='dodge', fill='steel blue') +
             xlab('Indel Length (bp)') + ylab('Number of INDELs') +
-            #xlim(0, 20) +
+            xlim(0, 20) +
             theme_bw()
 
 ggsave(png_out, len_plot, height=6, width=9)
